c6c8949eb28364530c56fc3d3999b5552bcf843d
sasiru <it22136824@my.sliit.lk>
2026-05-06 16:12:04 +0530

    add gitignore and deploy files

---
+++ deploy.py
@@ -0,0 +1,20 @@
+from huggingface_hub import HfApi, login
+
+login(token=os.environ["HF_TOKEN"])
+api = HfApi()
+
+# api.upload_folder(
+#     folder_path="./backend",
+#     repo_id="sas1ru/neuroheaven-backend",
+#     repo_type="space",
+#     ignore_patterns=["__pycache__", "*.pyc", ".env"]
+# )
+
+api.upload_folder(
+    folder_path="./frontend",
+    repo_id="sas1ru/neuroheaven-frontend",
+    repo_type="space",
+    ignore_patterns=["__pycache__", "*.pyc", ".env"]
+)
+
+print("Deployment complete!")
