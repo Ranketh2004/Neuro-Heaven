cc133d684a029f05240e387a0cf7b32bd1aaff80
sasiru <it22136824@my.sliit.lk>
2026-05-06 17:47:40 +0530

    fix deploy.py file

--- deploy.py
+++ deploy.py
@@ -1,20 +1,21 @@
+import os
 from huggingface_hub import HfApi, login
 
 login(token=os.environ["HF_TOKEN"])
 api = HfApi()
 
-# api.upload_folder(
-#     folder_path="./backend",
-#     repo_id="sas1ru/neuroheaven-backend",
-#     repo_type="space",
-#     ignore_patterns=["__pycache__", "*.pyc", ".env"]
-# )
-
 api.upload_folder(
-    folder_path="./frontend",
-    repo_id="sas1ru/neuroheaven-frontend",
+    folder_path="./backend",
+    repo_id="sas1ru/neuroheaven-backend",
     repo_type="space",
     ignore_patterns=["__pycache__", "*.pyc", ".env"]
 )
+
+# api.upload_folder(
+#     folder_path="./frontend",
+#     repo_id="sas1ru/neuroheaven-frontend",
+#     repo_type="space",
+#     ignore_patterns=["__pycache__", "*.pyc", ".env"]
+# )
 
 print("Deployment complete!")
